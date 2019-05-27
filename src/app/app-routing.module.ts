23ee1bd507312df57ec59c215c79031f5253b227
Rajeev Kumar <RThakur@delta.org>
2019-05-27 15:50:43 +0530

    Updated folder strcture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/Home/home.component';
+import { AboutComponent } from './components/About/about.component';
 
 const routes: Routes = [
-    {
-        path: 'home',
-        component: HomeComponent
-    }
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { StudentModule } from './student/student.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/Header/header.component';
+import { HomeComponent } from './components/Home/home.component';
+import { AboutComponent } from './components/About/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    StudentModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
